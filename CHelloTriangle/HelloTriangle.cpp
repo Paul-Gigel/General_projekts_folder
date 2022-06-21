8a7552e92213a1f4c4240bd56a25af5395b3c41c
Paul Gigel <p.gigel@bbw-fi.de>
2022-06-21 12:49:41 +0200

    HelloTriangle

---
+++ CHelloTriangle/HelloTriangle.cpp
@@ -0,0 +1,15 @@
+#include <vulkan/vulkan.h>
+
+#include <iostream>
+#include <stdexcept>
+#include <cstdlib>
+
+class HelloTriangleApplication  {
+public:
+    void run()  {
+
+    }
+};
+int main()  {
+    return 0;
+}
