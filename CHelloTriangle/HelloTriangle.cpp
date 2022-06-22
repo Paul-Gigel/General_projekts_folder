0981925a2c50d7cfe9b35256d6b8c721bb3b4457
Paul Gigel <p.gigel@bbw-fi.de>
2022-06-22 11:49:59 +0200

    validationlayers

--- CHelloTriangle/HelloTriangle.cpp
+++ CHelloTriangle/HelloTriangle.cpp
@@ -8,6 +8,33 @@
 const uint32_t WIDTH = 800;
 const uint32_t HEIGHT = 600;
 
+const std::vector<const char*> validationLayers = {
+        "VK_LAYER_KHRONOS_validation"
+};
+#ifdef NDEBUG
+    const bool enableValidationLayers = false;
+#else
+    const bool enableValidationLayers = true;
+#endif
+bool checkValidationLayerSupport()  {
+    uint32_t layerCount;
+    vkEnumerateInstanceLayerProperties(&layerCount, nullptr);
+    std::vector<VkLayerProperties> availableLayers(layerCount);
+    vkEnumerateInstanceLayerProperties(&layerCount, availableLayers.data());
+    for(const char* layerName : validationLayers)   {
+        bool layerFound = false;
+
+        for(const auto& layerProperties : availableLayers)  {
+            if(strcmp(layerName, layerProperties.layerName)== 0)    {
+                break;
+            }
+            if(!layerFound) {
+                return false;
+            }
+        }
+    }
+}
+
 class HelloTriangleApplication  {
 public:
     void run()  {
